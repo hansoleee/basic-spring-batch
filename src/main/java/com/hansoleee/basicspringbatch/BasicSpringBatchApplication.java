663439b8e361ae66551e201ac6fd7f15804a1f7e
lhs <leehansol.kr@gmail.com>
2021-08-28 11:48:23 +0900

    Project 생성

---
+++ src/main/java/com/hansoleee/basicspringbatch/BasicSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.hansoleee.basicspringbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSpringBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicSpringBatchApplication.class, args);
+    }
+
+}
